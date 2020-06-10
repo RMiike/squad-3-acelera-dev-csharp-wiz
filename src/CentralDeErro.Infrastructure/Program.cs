13c7bbb65d6047fe0d3d37dc55e04b4ba5bc597d
Renato Miike <miike_miike@hotmail.com>
2020-06-10 14:22:06 -0300

    base architecture

---
+++ src/CentralDeErro.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CentralDeErro.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
