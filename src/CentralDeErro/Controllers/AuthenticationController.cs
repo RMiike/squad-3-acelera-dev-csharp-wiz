6e32c788d461e53c88aa6044c1e769a9cf41c805
Renato Miike <miike_miike@hotmail.com>
2020-06-10 15:04:48 -0300

    authentication controller

---
+++ src/CentralDeErro/Controllers/AuthenticationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CentralDeErro.Core.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralDeErro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        // GET: api/Authentication]
+        //test route 
+        //return new User
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new User());
+        }
+
+        // GET: api/Authentication/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Authentication
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Authentication/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
