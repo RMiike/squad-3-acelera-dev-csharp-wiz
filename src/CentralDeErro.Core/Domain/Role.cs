84cbb7387712d8f4fa2dbfe7a4352ce8feda576a
Renato Miike <miike_miike@hotmail.com>
2020-06-10 14:27:23 -0300

    domain - user, role, userroles

---
+++ src/CentralDeErro.Core/Domain/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CentralDeErro.Core.Domain
+{
+    public class Role : IdentityRole
+    {
+    }
+}
---
+++ src/CentralDeErro.Core/Domain/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace CentralDeErro.Core.Domain
+{
+    class UserRoles : IdentityUserRole<string>
+    {
+    }
+}
